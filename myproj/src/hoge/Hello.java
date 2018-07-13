25222cd10c0682f7c95719c6d1b3e843826fe11a
ichiaki <ichiaki@jp.ibm.com>
2018-07-13 10:27:38 +0900

    コミットテスト

---
+++ myproj/src/hoge/Hello.java
@@ -0,0 +1,11 @@
+package hoge;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.println("Hello Test");
+		System.out.println("Hello Test2");
+	}
+
+}
